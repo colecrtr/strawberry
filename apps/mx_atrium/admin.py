601305809c5f3161e5b6a936e0045b9357ffc94c
Cole Carter <coleccarter@gmail.com>
2020-11-14 12:04:58 -0700

    Define initial MX Atrium User admin

--- apps/mx_atrium/admin.py
+++ apps/mx_atrium/admin.py
@@ -5,4 +5,5 @@ from . import models
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["account_user", "guid", "is_disabled"]
+    readonly_fields = ["account_user", "guid", "is_disabled"]
