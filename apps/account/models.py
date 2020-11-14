4db5715d063fa0c216787d2a25226c1ae1478722
Cole Carter <coleccarter@gmail.com>
2020-11-14 12:05:25 -0700

    Include BaseModel in auth.User

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 
+from project.models import BaseModel
 
-class User(AbstractUser):
+
+class User(AbstractUser, BaseModel):
     """Users of this application (auth.User)"""
 
     pass
